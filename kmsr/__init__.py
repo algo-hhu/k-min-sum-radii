da40df3f126616d175942dc1f0b9a656251b9d40
Giulia Baldini <giulia.baldini.11@gmail.com>
2024-07-19 07:35:51 +0200

    Only make class available from the outside

--- kmsr/__init__.py
+++ kmsr/__init__.py
@@ -1,5 +1,3 @@
 from kmsr.core import KMSR
-from kmsr.generate_points import generate_clusters
-from kmsr.plot import plot_multiple_results, plot_result
 
-__all__ = ["generate_clusters", "KMSR", "plot_result", "plot_multiple_results"]
+__all__ = ["KMSR"]
