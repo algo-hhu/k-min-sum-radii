dfc10c53d806493a31b43345d2118303373db060
Giulia Baldini <giulia.baldini.11@gmail.com>
2024-07-19 07:31:54 +0200

    Add plots

--- kmsr/plot.py
+++ kmsr/plot.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import List, Optional, Sequence
+from typing import Any, List, Optional, Sequence
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -14,16 +14,23 @@ def plot_2d_ax(
     centers: Optional[Sequence[Sequence[float]]] = None,
     radii: Optional[Sequence[float]] = None,
     title: Optional[str] = None,
+    edge_color: str = "black",
 ) -> None:
-    ax.scatter(points[:, 0], points[:, 1], c=clusters, s=50, cmap="Set2")
+    ax.scatter(points[:, 0], points[:, 1], c=clusters, s=20, cmap="Set2")
+    _centers = np.array(centers)
+    ax.scatter(_centers[:, 0], _centers[:, 1], c="red", s=50, alpha=0.5, marker="X")
 
     if centers is not None and radii is not None:
         for i, ((x, y), radius) in enumerate(zip(centers, radii)):
             circle = Circle(
-                (x, y), radius, fill=False, edgecolor="black", linestyle="--"
+                (x, y),
+                radius,
+                fill=False,
+                edgecolor=edge_color,
+                linestyle="--",
+                alpha=0.75,
             )
             ax.add_patch(circle)
-            ax.plot(x, y, "+", color="black")
 
             if clusters is not None:
                 _clusters = np.array(clusters)
@@ -35,7 +42,7 @@ def plot_2d_ax(
                     plt.Line2D(
                         [centers[i][0], furthest_point[0]],
                         [centers[i][1], furthest_point[1]],
-                        color="black",
+                        color=edge_color,
                         linestyle="--",
                     )
                 )
@@ -45,6 +52,77 @@ def plot_2d_ax(
     ax.set_ylabel("Feature 2")
 
     ax.set_aspect("equal")
+
+
+def plot_3d_ax(
+    points: np.ndarray,
+    ax: plt.Axes,
+    clusters: Optional[Sequence[int]] = None,
+    centers: Optional[Sequence[Sequence[float]]] = None,
+    radii: Optional[Sequence[float]] = None,
+    title: Optional[str] = None,
+    edge_color: str = "black",
+) -> None:
+    ax.scatter(points[:, 0], points[:, 1], points[:, 2], c=clusters, s=20, cmap="Set2")
+    _centers = np.array(centers)
+    ax.scatter(
+        _centers[:, 0],
+        _centers[:, 1],
+        _centers[:, 2],
+        c="red",
+        s=50,
+        alpha=0.5,
+        marker="X",
+    )
+
+    if centers is not None and radii is not None:
+        for i, ((c_x, c_y, c_z), radius) in enumerate(zip(centers, radii)):
+            u, v = np.mgrid[0 : 2 * np.pi : 20j, 0 : np.pi : 10j]  # type: ignore
+            x = c_x + radius * np.cos(u) * np.sin(v)
+            y = c_y + radius * np.sin(u) * np.sin(v)
+            z = c_z + radius * np.cos(v)
+            ax.plot_wireframe(x, y, z, linestyle="--", color=edge_color, alpha=0.3)
+
+            if clusters is not None:
+                _clusters = np.array(clusters)
+                points_in_cluster = points[_clusters == i]
+                furthest_point = points_in_cluster[
+                    np.argmax(np.linalg.norm(points_in_cluster - centers[i], axis=1))
+                ]
+
+                ax.plot(
+                    [_centers[i, 0], furthest_point[0]],
+                    [_centers[i, 1], furthest_point[1]],
+                    [_centers[i, 2], furthest_point[2]],
+                    color=edge_color,
+                    linestyle="--",
+                )
+    if title is not None:
+        ax.set_title(title)
+
+    ax.set_xlabel("Feature 1")
+    ax.set_ylabel("Feature 2")
+    ax.set_zlabel("Feature 3")
+
+    ax.set_aspect("equal")
+
+
+def plot_ax(points: np.ndarray, *args: Any, **kwargs: Any) -> None:
+    if len(points[0]) == 2:
+        plot_2d_ax(points, *args, **kwargs)
+    elif len(points[0]) == 3:
+        plot_3d_ax(points, *args, **kwargs)
+    else:
+        raise ValueError("Only 2D and 3D data is supported for plotting.")
+
+
+def get_projection(dim: int) -> Optional[str]:
+    if dim == 2:
+        return None
+    elif dim == 3:
+        return "3d"
+    else:
+        raise ValueError("Only 2D and 3D data is supported for plotting.")
 
 
 def plot_result(
@@ -57,12 +135,11 @@ def plot_result(
     show: bool = True,
     transparent: bool = True,
 ) -> None:
-    if len(points[0]) > 2:
-        raise ValueError("Only 2D data is supported for plotting.")
+    fig, ax = plt.subplots(
+        figsize=(10, 10), subplot_kw=dict(projection=get_projection(len(points[0])))
+    )
 
-    fig, ax = plt.subplots(figsize=(10, 10))
-
-    plot_2d_ax(np.array(points), ax, clusters, centers, radii, title)
+    plot_ax(np.array(points), ax, clusters, centers, radii, title)
 
     if output_path is not None:
         plt.savefig(
@@ -87,8 +164,7 @@ def plot_multiple_results(
     show: bool = True,
     transparent: bool = True,
 ) -> None:
-    if len(points[0]) > 2:
-        raise ValueError("Only 2D data is supported for plotting.")
+    proj = get_projection(len(points[0]))
 
     if clusterings is not None:
         ll = len(clusterings)
@@ -100,10 +176,15 @@ def plot_multiple_results(
         ll = 1
 
     if ll == 1:
-        fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+        fig, ax = plt.subplots(1, 1, figsize=(10, 10), subplot_kw=dict(projection=proj))
         axs: List[Axes] = [ax]
     else:
-        fig, axs = plt.subplots(1, ll, figsize=(10 * ll, 10))  # type: ignore
+        fig, axs = plt.subplots(  # type: ignore
+            1,
+            ll,
+            figsize=(10 * ll, 10),
+            subplot_kw=dict(projection=proj),
+        )
 
     if clusterings is None:
         clusterings = [None] * ll
@@ -118,7 +199,7 @@ def plot_multiple_results(
         titles = [f"Plot {i}" for i in range(ll)]
 
     for params in zip(axs, clusterings, centers, radii, titles):
-        plot_2d_ax(np.array(points), *params)
+        plot_ax(np.array(points), *params)
 
     if output_path is not None:
         plt.savefig(
